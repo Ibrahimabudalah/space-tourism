f904ccc0b484b2f8d6a9d6816ccdd4ff2e389470
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-01-25 19:44:28 -0800

    created the header component, installed sass, and created/imported the partials

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import React from "react";
+import Header from './components/header/header'
+import './App.scss'
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Header />
+  </div>;
 }
 
 export default App;
