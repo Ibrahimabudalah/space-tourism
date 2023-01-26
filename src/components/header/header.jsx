4eab22971046bba2e84cd4830efe629f7e3b681f
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-01-26 01:02:11 -0800

    imported and created the hamburger menu icon (no functionality)

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import './header.scss'
+import React from "react";
+import "./header.scss";
+import logo from "../../assets/icons/logo.svg";
+import hamburgerMenu from '../../assets/icons/icon-hamburger.svg'
 
 function header() {
-  return (
-    <div>header</div>
-  )
+    return <div className="header">
+        <img src={logo} className="header__logo" alt="logo" />
+        <img src={hamburgerMenu} className="header__menu" alt="hamburger-menu" />
+  </div>;
 }
 
-export default header
+export default header;
