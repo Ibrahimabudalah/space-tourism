d9cfc5825de771cf8b2c7fb40d763ebc3f8a17b7
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-01-26 01:45:04 -0800

    added the closed menu icon and created functions to handle the toggle between the two icons when clicked

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.scss";
 import logo from "../../assets/icons/logo.svg";
-import hamburgerMenu from '../../assets/icons/icon-hamburger.svg'
+import hamburgerMenu from "../../assets/icons/icon-hamburger.svg";
+import xMenu from "../../assets/icons/icon-close.svg";
 
-function header() {
-    return <div className="header">
-        <img src={logo} className="header__logo" alt="logo" />
-        <img src={hamburgerMenu} className="header__menu" alt="hamburger-menu" />
-  </div>;
+function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const menuClose = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <div className="header">
+      <img src={logo} className="header__logo" alt="logo" />
+      <div onClick={menuToggle} className="header__container">
+        {menuOpen ? (
+          <img
+            src={xMenu}
+            alt="menu closed"
+            className="header__menu"
+            onClick={menuClose}
+          />
+        ) : (
+          <img
+            src={hamburgerMenu}
+            className="header__menu"
+            alt="hamburger-menu"
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default header;
+export default Header;
