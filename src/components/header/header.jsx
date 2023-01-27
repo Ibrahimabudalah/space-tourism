b56222f3fabc77c95b3be905077ce2e7ca310c5a
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-01-26 23:38:52 -0800

    created a utils file and mapped through it to display the pages titles in the menu list, built the logic that allows the list to show only when the toggle fuction is true + basic styling

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,6 +3,7 @@ import "./header.scss";
 import logo from "../../assets/icons/logo.svg";
 import hamburgerMenu from "../../assets/icons/icon-hamburger.svg";
 import xMenu from "../../assets/icons/icon-close.svg";
+import {pages} from "./utils";
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -32,6 +33,12 @@ function Header() {
             alt="hamburger-menu"
           />
         )}
+
+        <ul className={`header__list ${!menuOpen ? "header__list--show" : ""}`}>
+          {pages.map((item, index) => {
+            return <li className={item.cName} key={index}>{item.number + item.title}</li>;
+          })}
+        </ul>
       </div>
     </div>
   );
---
+++ src/components/header/utils.jsx
@@ -0,0 +1,26 @@
+export const pages = [
+  {
+    title: " HOME",
+    number: "00",
+    path: "/",
+    cName: "nav__item",
+  },
+  {
+    title: " DESTINATION",
+    number: "01",
+    path: "",
+    cName: "nav__item",
+  },
+  {
+    title: " CREW",
+    number: "02",
+    path: "",
+    cName: "nav__item",
+  },
+  {
+    title: " TECHNOLOGY",
+    number: "03",
+    path: "",
+    cName: "nav__item",
+  },
+];
